b949f40fd77c5f3084f2797e954f6618f5bb726c
Isabella Lopes <icmlopes@gmail.com>
2022-11-23 15:48:20 -0300

    feat: signUp working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import express from "express";
+import router from "./routes/signUpRoutes.js"
 import cors from "cors";
-import { MongoClient } from "mongodb";
+import express from "express";
 import dotenv from "dotenv";
+import joi from "joi";
+
+dotenv.config()
+
+import signUpRouters from "./routes/signUpRoutes.js"
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(router)
+app.use(signUpRouters)
 
-
-app.listen(process.env.PORT, () => {
-    console.log('server online, port' + process.env.PORT);
+export const userSchema = joi.object({
+    name: joi.string().min(3).required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(3).required(),
 })
+
+app.listen(4000, () => console.log("Server running in port: 4000"))
+// app.listen(process.env.PORT, () => {
+//     console.log('server online, port' + process.env.PORT);
+// })
 
 // conta do render  user: admin  senha: admin123
