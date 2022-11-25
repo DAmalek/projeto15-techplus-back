c4a6a5e1c1f3d296fb573b72007f9b9cd9992a73
Isabella Lopes <icmlopes@gmail.com>
2022-11-24 23:32:14 -0300

    feat: checking e-mail

--- src/controllers/signUpController.js
+++ src/controllers/signUpController.js
@@ -3,10 +3,18 @@ import bcrypt from "bcrypt";
 
 export async function postSignUp(req, res) {
   //nome, e-mail e senha
-  const user = res.locals.user;
+  const user = req.body
   const passwordHashed = bcrypt.hashSync(user.password, 10);
 
   try {
+
+    const emailExists = await usersCollection.findOne({ email: user.email })
+    if (emailExists) {
+      return res.status(400)
+        .send({
+          message: "Este e-mail já está sendo utilizado."
+        })
+    }
     await usersCollection.insertOne({
       ...user,
       password: passwordHashed,
